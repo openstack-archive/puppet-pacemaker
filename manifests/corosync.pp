a6f7118d38741d39376d5bfeaabc43a904b30900
Dan Radez <dradez@redhat.com>
2013-08-08 08:42:06 -0400

    removing ccs

--- manifests/corosync.pp
+++ manifests/corosync.pp
@@ -1,7 +1,7 @@
 define pacemaker::corosync() {
 
     package {["pacemaker", "pcs", "cman", "corosync",
-              "ccs", "pacemaker-cli"]:
+              "pacemaker-cli"]:
 	    ensure  => "installed",
     }
 
@@ -13,23 +13,23 @@ define pacemaker::corosync() {
 
     exec {"Create Cluster":
         creates => "/etc/cluster/cluster.conf",
-        command => "/usr/sbin/ccs -f /etc/cluster/cluster.conf --createcluster $name",
-        require => Package["ccs"], 
+        command => "/usr/sbin/pcs -f /etc/cluster/cluster.conf --createcluster $name",
+        require => Package["pcs"], 
     }
 
     exec {"Disable RGManager":
         subscribe   => Exec["Create Cluster"],
         refreshonly => true,
-        command     => "/usr/sbin/ccs -f /etc/cluster/cluster.conf --setrm disabled=1",
-        require     => [Package["ccs"], Exec["Create Cluster"]],
+        command     => "/usr/sbin/pcs -f /etc/cluster/cluster.conf --setrm disabled=1",
+        require     => [Package["pcs"], Exec["Create Cluster"]],
     }
 
     exec {"Setup Pacemaker Fencing Device":
         subscribe   => Exec["Create Cluster"],
         refreshonly => true,
-        command     => "/usr/sbin/ccs -f /etc/cluster/cluster.conf --addfencedev pcmk-redirect agent=fence_pcmk",
-        unless      => "/usr/sbin/ccs -f /etc/cluster/cluster.conf --lsfencedev | grep 'pcmk-redirect: agent=fence_pcmk'",
-        require     => [Package["ccs"], Exec["Create Cluster"]],
+        command     => "/usr/sbin/pcs -f /etc/cluster/cluster.conf --addfencedev pcmk-redirect agent=fence_pcmk",
+        unless      => "/usr/sbin/pcs -f /etc/cluster/cluster.conf --lsfencedev | grep 'pcmk-redirect: agent=fence_pcmk'",
+        require     => [Package["pcs"], Exec["Create Cluster"]],
     }
 
     service { "cman":
@@ -48,25 +48,25 @@ define pacemaker::corosync() {
 define pacemaker::corosync::node() {
 
     exec {"Create Node $name":
-        command => "/usr/sbin/ccs -f /etc/cluster/cluster.conf --addnode $name",
-        unless  => "/usr/sbin/ccs -f /etc/cluster/cluster.conf --lsnodes | grep $name:",
-        require => [Package["ccs"], Exec["Create Cluster"]],
+        command => "/usr/sbin/pcs -f /etc/cluster/cluster.conf --addnode $name",
+        unless  => "/usr/sbin/pcs -f /etc/cluster/cluster.conf --lsnodes | grep $name:",
+        require => [Package["pcs"], Exec["Create Cluster"]],
         notify  => Service["cman"],
     }
 
     exec {"Add Fencing Redirect Method to Node $name":
         subscribe => Exec["Create Node $name"],
         #refreshonly => true,
-        command => "/usr/sbin/ccs -f /etc/cluster/cluster.conf --addmethod pcmk-redirect $name",
-        unless  => "/usr/sbin/ccs -f /etc/cluster/cluster.conf --lsfenceinst $name | grep '  pcmk-redirect'",
-        require => [Package["ccs"],Exec["Create Node $name"]]
+        command => "/usr/sbin/pcs -f /etc/cluster/cluster.conf --addmethod pcmk-redirect $name",
+        unless  => "/usr/sbin/pcs -f /etc/cluster/cluster.conf --lsfenceinst $name | grep '  pcmk-redirect'",
+        require => [Package["pcs"],Exec["Create Node $name"]]
     }
 
     exec {"Activate Fencing Redirect for Node $name":
         subscribe => Exec["Create Node $name"],
         #refreshonly => true,
-        command => "/usr/sbin/ccs -f /etc/cluster/cluster.conf --addfenceinst pcmk-redirect $name pcmk-redirect port=$name",
-        unless  => "/usr/sbin/ccs -f /etc/cluster/cluster.conf --lsfenceinst $name | grep 'pcmk-redirect: port=$name'",
-        require => [Package["ccs"], Exec["Add Fencing Redirect Method to Node $name"]]
+        command => "/usr/sbin/pcs -f /etc/cluster/cluster.conf --addfenceinst pcmk-redirect $name pcmk-redirect port=$name",
+        unless  => "/usr/sbin/pcs -f /etc/cluster/cluster.conf --lsfenceinst $name | grep 'pcmk-redirect: port=$name'",
+        require => [Package["pcs"], Exec["Add Fencing Redirect Method to Node $name"]]
     }
 }
