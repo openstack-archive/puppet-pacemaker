b8341743533469f068943d2c25783ca72053636e
Dan Radez <dradez@redhat.com>
2013-10-01 13:24:38 -0400

    syntax fix in the group resource handling

--- manifests/resource/group.pp
+++ manifests/resource/group.pp
@@ -2,7 +2,7 @@ define pacemaker::resource::group($resource_id, $resource_group) {
     if($group != nil) {
         exec { "Adding $resource_id to $resource_group":
             command => "/usr/sbin/pcs resource group add ${resource_group} ${resource_id}",
-            onlyif  => "/usr/sbin/pcs resource group show ${resource_group} | grep 'Resource: ${resource_group}' > /dev/null 2>&1",
+            onlyif  => "/usr/sbin/pcs resource show ${resource_group} | grep 'Resource: ${resource_group}' > /dev/null 2>&1",
         }
     }
 }
