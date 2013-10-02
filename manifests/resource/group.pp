ed4594be496d95c26e97451a38d1db47e9747432
Crag Wolfe <cwolfe@redhat.com>
2013-10-02 02:13:28 -0400

    update condition to add a resource to resource group

--- manifests/resource/group.pp
+++ manifests/resource/group.pp
@@ -2,8 +2,7 @@ define pacemaker::resource::group($resource_id, $resource_group) {
     if($group != nil) {
         exec { "Adding $resource_id to $resource_group":
             command => "/usr/sbin/pcs resource group add ${resource_group} ${resource_id}",
-            onlyif  => "/usr/sbin/pcs resource show ${resource_group} | grep 'Resource: ${resource_group}' > /dev/null 2>&1",
+            unless => "/usr/sbin/pcs resource show ${resource_group} | grep -q 'Resource: ${resource_id}' > /dev/null 2>&1",
         }
     }
 }
-
