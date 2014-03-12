0dd816a703bb16e0d67a32422a771e7aad2e155c
Crag Wolfe <cwolfe@redhat.com>
2014-03-12 00:56:43 -0400

    Update impilan fencing, follow the pattern of fence_xvm.

--- manifests/stonith/ipmilan.pp
+++ manifests/stonith/ipmilan.pp
@@ -1,20 +1,38 @@
-class pacemaker::stonith::ipmilan($address, $user="stonith", $password, $no_location_rule="false", $cloned="false", $ensure=present)
-  inherits pacemaker::corosync {
-    package { "ipmitool":
-        ensure => installed,
+class pacemaker::stonith::ipmilan(
+  $address        ="10.10.10.1",
+  $username       ="",
+  $password       ="",
+  $interval       ="60s",
+  $ensure         ="present",
+  $pcmk_host_list ="",
+  ) {
+  if($ensure == absent) {
+    exec { "Removing stonith::ipmilan ${address}":
+      command => "/usr/sbin/pcs stonith delete stonith-ipmilan-${address}",
+      onlyif  => "/usr/sbin/pcs stonith show stonith-ipmilan-${address} > /dev/null 2>&1",
+      require => Class['pacemaker::corosync'],
+    }
+  } else {
+    $username_chunk = $username ? {
+      ''      => '',
+      default => "login=${username}",
+    }
+    $password_chunk = $password ? {
+      ''      => '',
+      default => "passwd=${password}",
+    }
+    $pcmk_host_list_chunk = $pcmk_host_list ? {
+      ''      => '',
+      default => "pcmk_host_list=\"${pcmk_host_list}\"",
     }
 
-    if($ensure == absent) {
-        exec { "Removing stonith::ipmilan ${address}":
-        command => "/usr/sbin/pcs stonith delete stonith-ipmilan-${address}",
-        onlyif  => "/usr/sbin/pcs stonith show stonith-ipmilan-${address} > /dev/null 2>&1",
-        require => Exec["Start Cluster $cluster_name"],
-        }
-    } else {
-        exec { "Creating stonith::ipmilan ${address}":
-        command => "/usr/sbin/pcs stonith create stonith-ipmilan-${address} fence_ipmilan pcmk_host_list=\"${cluster_members}\" ipaddr=${address} login=${user} passwd=${password} op monitor interval=20s",
-        unless  => "/usr/sbin/pcs stonith show stonith-ipmilan-${address} > /dev/null 2>&1",
-        require => [Package["ipmitool"],Exec["Start Cluster $cluster_name"],Package["pcs"]]
-        }
+    package { "ipmitool":
+      ensure => installed,
+    } ->
+    exec { "Creating stonith::ipmilan ${address}":
+      command => "/usr/sbin/pcs stonith create stonith-ipmilan-${address} fence_ipmilan ${pcmk_host_list_chunk} ipaddr=${address} ${username_chunk} ${password_chunk} op monitor interval=${interval}",
+      unless  => "/usr/sbin/pcs stonith show stonith-ipmilan-${address} > /dev/null 2>&1",
+      require => Class['pacemaker::corosync'],
     }
+  }
 }
