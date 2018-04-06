da4673ffbeb43c426639b2a4982d6f80557c8d6a
Michele Baldessari <michele@acksyn.org>
2018-04-06 13:40:29 +0200

    Introduce some new functions in pcmk_common
    We hereby introduce some new utility functions:
    A) pcs_offline() which will be used to run pcs commands without retry
       on a CIB file
    B) push_cib_offline() which is used to try and push a cib that was
       manipulated with pcs_offline() back to the live cluster. It will
       try a certain specified amount of times before giving up.
    C) pcmk_graph_contain_id?() which takes a resource_name and a graph
       file generated via 'crm_simulate -G<file>' and will return true
       if the resource is going to be restarted by pacemaker and false if
       otherwise. This is used so that we can determine if pacemaker deems
       that a resource has changed or not.
    D) pcmk_restart_resource?() which, given a resource_name and a cib will
       return true if pacemaker plans to restart the resource and false if
       it does not. Due to https://bugzilla.redhat.com/show_bug.cgi?id=1561617
       we leverage crm_simulate and not crm_diff
    E) pcmk_resource_has_changed?() which will delete the resource from an
       offline CIB copy, re-add it and then verify via
       pcmk_restart_resource?() if a restart would take place. The reason we
       need to remove a resource and then re-add it is due to the way pcs
       commands around bundles are limited (you basically can't update a
       bundle with the list of all all the storage-mappings: you have to specify which
       mappings are going to be added and which removed). So the simplest
       way is to just remove the resource and re-add it.
    
    Change-Id: Iba7092b79af3a4dbc2888f35504175d5eb0abcdb

---
+++ spec/unit/puppet/provider/pcmk_common_spec.rb
@@ -0,0 +1,59 @@
+require 'spec_helper'
+
+require_relative '../../../../lib/puppet/provider/pcmk_common'
+
+describe "pcmk_common functions" do
+  before(:all) do
+    # FIXME: we need to stub this properly not via this hack
+    PCMK_TMP_BASE = "/tmp"
+    orig_cib = File.join File.dirname(__FILE__), 'cib-orig.xml'
+    FileUtils.cp orig_cib, "cib-noop.xml"
+    FileUtils.cp orig_cib, "cib-resource.xml"
+    FileUtils.cp orig_cib, "cib-bundle.xml"
+  end
+
+  it "pcmk_graph_contain_id? raises proper exception" do
+    expect { pcmk_graph_contain_id?('foo', 'bar') }.to raise_error(Errno::ENOENT)
+  end
+
+  it "pcs_offline noop update" do
+    expect(pcs_offline('resource update ip-172.16.11.97 cidr_netmask=32', 'cib-noop.xml')).to eq ""
+  end
+  it "pcmk_restart_resource? noop" do
+    expect(pcmk_restart_resource?('foo', "cib-noop.xml")).to eq false
+    expect(pcmk_restart_resource?('ip-172.16.11.97', "cib-noop.xml")).to eq false
+  end
+
+  it "pcs_offline update to resource definition" do
+    expect(pcs_offline('resource update ip-172.16.11.97 cidr_netmask=31', 'cib-resource.xml')).to eq ""
+  end
+  it "pcmk_restart_resource? vip resource" do
+    expect(pcmk_restart_resource?('foo', "cib-resource.xml")).to eq false
+    expect(pcmk_restart_resource?('ip-172.16.11.97', "cib-resource.xml")).to eq true
+  end
+
+  it "pcs_offline update to bundle definition" do
+    # We effectively change the number of replicas from 3 to 2
+    expect(pcs_offline('resource delete test_bundle', 'cib-bundle.xml')).not_to eq false
+    cmd = 'resource bundle create test_bundle container docker image=docker.io/sdelrio/docker-minimal-nginx '\
+          'replicas=2 options="--user=root --log-driver=journald -e KOLLA_CONFIG_STRATEGY=COPY_ALWAYS" '\
+          'network=host storage-map id=haproxy-cfg-files source-dir=/var/lib/kolla/config_files/haproxy.json '\
+          'target-dir=/var/lib/kolla/config_files/config.json options=ro storage-map id=haproxy-cfg-data '\
+          'source-dir=/var/lib/config-data/puppet-generated/haproxy/ target-dir=/var/lib/kolla/config_files/src '\
+          'options=ro storage-map id=haproxy-hosts source-dir=/etc/hosts target-dir=/etc/hosts options=ro '\
+          'storage-map id=haproxy-localtime source-dir=/etc/localtime target-dir=/etc/localtime options=ro '\
+          'storage-map id=haproxy-pki-extracted source-dir=/etc/pki/ca-trust/extracted '\
+          'target-dir=/etc/pki/ca-trust/extracted options=ro storage-map id=haproxy-pki-ca-bundle-crt '\
+          'source-dir=/etc/pki/tls/certs/ca-bundle.crt target-dir=/etc/pki/tls/certs/ca-bundle.crt options=ro '\
+          'storage-map id=haproxy-pki-ca-bundle-trust-crt source-dir=/etc/pki/tls/certs/ca-bundle.trust.crt '\
+          'target-dir=/etc/pki/tls/certs/ca-bundle.trust.crt options=ro storage-map id=haproxy-pki-cert '\
+          'source-dir=/etc/pki/tls/cert.pem target-dir=/etc/pki/tls/cert.pem options=ro storage-map '\
+          'id=haproxy-dev-log source-dir=/dev/log target-dir=/dev/log options=rw'
+    expect(pcs_offline(cmd, 'cib-bundle.xml')).to eq ""
+  end
+
+  it "pcmk_restart_resource? bundle resource" do
+    expect(pcmk_restart_resource?('foo', "cib-bundle.xml", true)).to eq false
+    expect(pcmk_restart_resource?('test_bundle', "cib-bundle.xml", true)).to eq true
+  end
+end
